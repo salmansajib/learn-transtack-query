1db6d750d24f3c9484a9a54ab0a92baeb36608c5
Salman-sajib <sfsajib@gmail.com>
2024-08-17 13:39:12 +0600

    fetched data using transtack query

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import "./App.css";
 
+import { useQuery } from "@tanstack/react-query";
+
 function App() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["todo"],
+    queryFn: () =>
+      fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
+        res.json()
+      ),
+  });
+
+  if (error) return <div>There was an error!</div>;
+
+  if (isLoading) return <div>Loading... Please wait</div>;
+
   return (
     <div>
       <h1>Transtack Query</h1>
+      {data.map((todo) => (
+        <div key={todo.id}>
+          <h3>Id: {todo.id}</h3>
+          <h4>Title: {todo.title} </h4>
+        </div>
+      ))}
     </div>
   );
 }
