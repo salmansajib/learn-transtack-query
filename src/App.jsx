9aa8ef410c6476645fd318fe80b66b6181d43ab1
Salman-sajib <sfsajib@gmail.com>
2024-08-18 18:22:02 +0600

    fetch data and mutate it

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,54 @@
 import "./App.css";
 
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+
+const posts = [
+  { id: 1, title: "post 1" },
+  { id: 2, title: "post 2" },
+];
+
+function wait(duration) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
+}
 
 function App() {
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["todo"],
-    queryFn: () =>
-      fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
-        res.json()
-      ),
+  const queryClient = useQueryClient();
+
+  const postQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...posts]),
   });
 
-  if (error) return <div>There was an error!</div>;
+  const newPostMutation = useMutation({
+    mutationFn: (title) => {
+      return wait(1000).then(() =>
+        posts.push({ id: crypto.randomUUID(), title })
+      );
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"]);
+    },
+  });
 
-  if (isLoading) return <div>Loading... Please wait</div>;
+  if (postQuery.isLoading) return <h1>Loading...</h1>;
+  if (postQuery.isError) {
+    return <pre> {JSON.stringify(postQuery.error)} </pre>;
+  }
 
   return (
     <div>
-      <h1>Transtack Query</h1>
-      {data.map((todo) => (
-        <div key={todo.id}>
-          <h3>Id: {todo.id}</h3>
-          <h4>Title: {todo.title} </h4>
-        </div>
-      ))}
+      <h1>Tanstack Query</h1>
+      <div>
+        {postQuery.data.map((post) => (
+          <h3 key={post.id}>{post.title}</h3>
+        ))}
+        <button
+          disabled={newPostMutation.isLoading}
+          onClick={() => newPostMutation.mutate("new post")}
+        >
+          Add New
+        </button>
+      </div>
     </div>
   );
 }
